f3338c1acb8014925049378d921a3a9082cd5d29
BASIL LAL <112262693+basillal@users.noreply.github.com>
2024-06-23 22:20:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,49 +11,52 @@ const uri = "mongodb+srv://basillal1010:OrWVHT3y6Z8hXwPq@store.2p1clix.mongodb.n
 const client = new MongoClient(uri);
 
 // Function to run the database operation
-async function run() {
-  try {
-    // Connect to the MongoDB client
-    await client.connect();
+function run() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Connect to the MongoDB client
+      await client.connect();
+      
+      // Select the database and collection
+      const database = client.db('store');
+      console.log("db passed")
+      const movies = database.collection('movies');
+
+      const newMovie = {
+          title: 'Back to the Future',
+          year: 1985,
+          genre: ['Adventure', 'Comedy', 'Science Fiction'],
+          director: 'Robert Zemeckis',
+          actors: ['Michael J. Fox', 'Christopher Lloyd']
+        };
     
-    // Select the database and collection
-    const database = client.db('store');
-    console.log("db passed")
-    const movies = database.collection('movies');
+        const result = await movies.insertOne(newMovie);
+        console.log(`A movie was inserted with the following id: ${result.insertedId}`);
 
-    const newMovie = {
-        title: 'Back to the Future',
-        year: 1985,
-        genre: ['Adventure', 'Comedy', 'Science Fiction'],
-        director: 'Robert Zemeckis',
-        actors: ['Michael J. Fox', 'Christopher Lloyd']
-      };
-  
-      const result = await movies.insertOne(newMovie);
-      console.log(`A movie was inserted with the following id: ${result.insertedId}`);
+      // Perform the query
+      const query = { title: 'Back to the Future' };
+      const movie = await movies.findOne(query);
+      console.log(movie);
 
-    // Perform the query
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } catch (error) {
-    console.error("An error occurred:", error);
-  } finally {
-    // Close the connection to the MongoDB client
-    await client.close();
-  }
+      resolve(); // Resolve the promise when operation is successful
+    } catch (error) {
+      console.error("An error occurred:", error);
+      reject(error); // Reject the promise if there's an error
+    } finally {
+      // Close the connection to the MongoDB client
+      await client.close();
+    }
+  });
 }
 
 // Start the Express server
-app.get("/", async (req, res) => {
-  try {
-res.send("Express on Vercel");
-    await run();
-    
-  } catch (error) {
+app.get("/", (req, res) => {
+  run().then(() => {
+    res.send("Express on Vercel");
+  }).catch((error) => {
     console.error(error);
     res.status(500).send("Failed to fetch movie");
-  }
+  });
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
